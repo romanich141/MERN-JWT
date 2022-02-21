3f78972ab5afe93b47ddc4435a4e32fa2afa6939
roma_ovcharenko <roma_ovcharenko@ukr.net>
2022-02-21 21:05:48 +0200

    add connect mongo

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 require("dotenv").config();
+const connectDB = require("./config/db");
+const colors = require("colors");
 const PORT = process.env.PORT;
 const app = express();
-const { errorHandler } = require("../middleware/errorMiddleware");
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }))
-app.use("/api/goals", require("../routes/goalRoutes"));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server started in ${ PORT }`))
