9879a7e6ba975580b74f028bfac311c5419cdf48
roma_ovcharenko <roma_ovcharenko@ukr.net>
2022-02-21 20:34:34 +0200

    add error handler

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
+const { errorHandler } = require("../middleware/errorMiddleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }))
 app.use("/api/goals", require("../routes/goalRoutes"));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server started in ${ PORT }`))
